b89c3d30d091191a71392f0efa424d1fdad098c5
casstrops <kriss52450@gmail.com>
2023-11-23 01:26:14 +0530

    switch server to asyncio

--- src/server.py
+++ src/server.py
@@ -1,78 +1,45 @@
-import socket
-import threading
+import asyncio
 
-HOST = "127.0.0.1"
-PORT = 3005
+class Server:
+    def __init__(self):
+        self.clients = []
 
-def receive_and_broadcast_message(client_socket, client_address, active_client_sockets, active_client_addresses):
-    while True:
-
+    async def handle_client(self, reader, writer):
+        self.clients.append(writer)
         try:
-            message = client_socket.recv(1024).decode('utf-8')
-            if message == "!logout":
-                print(f"[CLOSED] : {client_address}")
-                break
-            print(f"[{client_address}]: {message}")
-            for active_client_socket in active_client_sockets:
-                if active_client_socket != client_socket:
-                    active_client_socket.send(("[" + str(client_address) + "]: " + message).encode('utf-8'))
+            while True:
+                data = await reader.read(200)
+                message = data.decode().strip()
+                if not message:
+                    break
+                print(f"Client: {message}")
+                await self.broadcast(message)
+        except asyncio.CancelledError:
+            pass
+        finally:
+            print("Client disconnected")
+            self.clients.remove(writer)
+            writer.close()
+            await writer.wait_closed()
 
-        except Exception as receive_message_exception:
-            print(f"An exception has occured on receiving message from {client_address}: {receive_message_exception}")
-         
-            active_client_sockets.remove(client_socket)
-            active_client_addresses.remove(client_address)
-            client_socket.close()
-            print(f"--- ACTIVE CLIENT SOCKETS: {len(active_client_sockets)} ---")
+    async def broadcast(self, message):
+        if self.clients:
+            print(f"Broadcast: {message}")
+            tasks = [client.write(message.encode()) for client in self.clients]
+            await asyncio.gather(*tasks)
 
-    active_client_sockets.remove(client_socket)
-    active_client_addresses.remove(client_address)
-    client_socket.close()
-    print(f"--- ACTIVE CLIENT SOCKETS: {len(active_client_sockets)} ---")    
+async def main():
+    server = Server()
+    server_address = ('localhost', 3005)
 
-    return
+    chat_server = await asyncio.start_server(
+        server.handle_client, *server_address
+    )
 
-def broadcast_message(active_client_sockets, active_client_addresses):
-    message = input("")
-    for active_client_socket in active_client_sockets:
-        active_client_socket.send(("[SERVER]: " + message).encode('utf-8'))
+    print(f"Server started on {server_address}")
 
-def start_server(host, port):
+    async with chat_server:
+        await chat_server.serve_forever()
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-
-    server_socket.listen(2)
-    print(f"[LISTENING] {host}:{port}")
-
-    active_client_sockets = []
-    active_client_addresses = []
-
-    while True:
-
-        try:
-            client_socket, client_address = server_socket.accept()         
-            print(f"Connection established with {client_address}")
-
-            active_client_sockets.append(client_socket)
-            active_client_addresses.append(client_address)
-
-            print(f"--- ACTIVE CLIENT SOCKETS: {len(active_client_sockets)} ---")
-
-            receive_and_broadcast_thread = threading.Thread(target = receive_and_broadcast_message, args = (client_socket, client_address, active_client_sockets, active_client_addresses))
-            broadcast_message_thread = threading.Thread(target = broadcast_message, args = (active_client_sockets, active_client_addresses))
-
-            receive_and_broadcast_thread.start()
-            broadcast_message_thread.start()
-
-        except Exception as server_start_exception:
-            print(f"An exception has occured on starting server: {server_start_exception}")
-
-    return
-
-if __name__ == "__main__":  
-
-    host = HOST
-    port = PORT
-
-    start_server(host, port)
+if __name__ == "__main__":
+    asyncio.run(main())
